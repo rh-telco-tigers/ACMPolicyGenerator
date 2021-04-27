224bc77ca3be4f2466d7a4c5671630770cf5e444
kcalliga <41206787+kcalliga@users.noreply.github.com>
2021-04-27 13:39:05 -0400

    Update ACMPolicyGenerator.sh

--- ACMPolicyGenerator.sh
+++ ACMPolicyGenerator.sh
@@ -5,7 +5,7 @@ for object in `ls object*`; do
 cat configuration-policy-template.yaml >> output.yaml
 echo "            objectDefinition:" >> output.yaml
 cat $object | paste /dev/null - | expand -15 >> output.yaml;
-echo "  - objectDefinition:" >> output.yaml;
+#echo "  - objectDefinition:" >> output.yaml;
 done;
 cat template-bottom.yaml >> output.yaml;
 
