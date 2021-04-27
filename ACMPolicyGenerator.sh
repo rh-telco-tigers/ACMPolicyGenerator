0e5ba75f886a5ad51c40e56c200d3d5c280d4e5a
kcalliga <41206787+kcalliga@users.noreply.github.com>
2021-04-27 13:42:39 -0400

    Update ACMPolicyGenerator.sh

--- ACMPolicyGenerator.sh
+++ ACMPolicyGenerator.sh
@@ -3,6 +3,7 @@
 cat template-top.yaml >> output.yaml
 for object in `ls object*`; do 
 cat configuration-policy-template.yaml >> output.yaml
+echo "          - complianceType: musthave"
 echo "            objectDefinition:" >> output.yaml
 cat $object | paste /dev/null - | expand -15 >> output.yaml;
 #echo "  - objectDefinition:" >> output.yaml;
